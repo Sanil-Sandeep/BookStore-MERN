c10a99ab57be8c5dac2ba255dff25be225e9993d
Sanil Sandeep <sanil.sandeep001@gmail.com>
2024-07-11 18:17:09 +0530

    Update

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://sanilsandeep:sanil2002@bookstore.bdrll6o.mongodb.net/BookStore?retryWrites=true&w=majority'
+export const mongoDBURL = 'mongodb+srv://sanilsandeep:sanil002200@bookstore.bdrll6o.mongodb.net/BookStore?retryWrites=true&w=majority'
